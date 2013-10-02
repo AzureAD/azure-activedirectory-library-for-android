168cc0dc3abab22c4353c86c176345c90958a768
Omer Cansizoglu <cansizoc@gmail.com>
2013-10-02 01:05:55 -0700

    one working test project that uses itself as instrumentation project to test library

---
+++ tests-test/src/com/microsoft/adal/test/instruments/AuthenticationTests.java
@@ -0,0 +1,13 @@
+package com.microsoft.adal.test.instruments;
+
+import com.microsoft.adal.AuthenticationResult;
+import com.microsoft.adal.AuthenticationResult.AuthenticationStatus;
+
+public class AuthenticationTests {
+
+	public AuthenticationResult testCtor() {
+
+		AuthenticationResult result = new AuthenticationResult();
+		return result;
+	}
+}
