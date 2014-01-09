dfb48b4be49b320f57bdfacb8056b62072a60235
omercs <cansizoc@gmail.com>
2014-01-09 10:25:31 -0800

    encrypt-decrypt using androidkeystore

--- tests/src/com/microsoft/adal/test/StorageHelperTests.java
+++ tests/src/com/microsoft/adal/test/StorageHelperTests.java
@@ -18,21 +18,39 @@ public class StorageHelperTests extends AndroidTestCase {
             InvocationTargetException {
         String clearText = "SomeValue1234";
         Log.d(TAG, "short text testEncryptDecrypt");
-        Constructor<?> constructorParams = Class.forName("com.microsoft.adal.StorageHelper").getDeclaredConstructor(Context.class);
-        constructorParams.setAccessible(true);
-        Object storageHelper = constructorParams.newInstance(getContext());
+        Object storageHelper = getStorageHelper();
         Method mEncrypt = ReflectionUtils.getTestMethod(storageHelper, "encrypt", String.class);
         Method mDecrypt = ReflectionUtils.getTestMethod(storageHelper, "decrypt", String.class);
         String encrypted = (String)mEncrypt.invoke(storageHelper, clearText);
         assertNotNull("encrypted string is not null", encrypted);
         assertFalse("encrypted string is not same as cleartex", encrypted.equals(clearText));
         String decrypted = (String)mDecrypt.invoke(storageHelper, encrypted);
-
         assertEquals("Same as initial text", clearText, decrypted);
-        
         Log.d(TAG, "Long text testEncryptDecrypt");
     }
 
-    
-    
+    public void testFixedPassword() throws IllegalArgumentException, ClassNotFoundException,
+            NoSuchMethodException, InstantiationException, IllegalAccessException,
+            InvocationTargetException {
+        Log.d(TAG, "short text testEncryptDecrypt");
+        Object storageHelper = getStorageHelper();
+        Method m = ReflectionUtils.getTestMethod(storageHelper, "getPassword");
+
+        byte[] passwordBytes = (byte[])m.invoke(storageHelper);
+
+        assertNotNull("encrypted string is not null", passwordBytes);
+        assertTrue("contains fixed info",
+                new String(passwordBytes).contains("B58AD341-0D4B-46B8-A81B-552B86051BDD"));
+
+    }
+
+    private Object getStorageHelper() throws NoSuchMethodException, ClassNotFoundException,
+            InstantiationException, IllegalAccessException, InvocationTargetException {
+        Constructor<?> constructorParams = Class.forName("com.microsoft.adal.StorageHelper")
+                .getDeclaredConstructor(Context.class);
+        constructorParams.setAccessible(true);
+        Object storageHelper = constructorParams.newInstance(getContext());
+        return storageHelper;
+    }
+
 }
