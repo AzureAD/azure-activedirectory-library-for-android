328ed17a42b1ead66f912b78dd4571d23daf8b94
omercs <cansizoc@gmail.com>
2014-02-10 13:37:59 -0800

    update travis.yml to post coverage report update sample constants

--- samples/webapi/src/com/example/com/microsoft/adal/hello/Constants.java
+++ samples/webapi/src/com/example/com/microsoft/adal/hello/Constants.java
@@ -14,19 +14,16 @@ public class Constants {
 	
 	
 	// -------------------------------AAD PARAMETERS----------------------------------
-//	static final String AUTHORITY_URL = "https://login.windows.net/omercantest.onmicrosoft.com";
-//	static final String CLIENT_ID = "650a6609-5463-4bc4-b7c6-19df7990a8bc"; 
-//	static final String RESOURCE_ID =  "https://omercantest.onmicrosoft.com/AllHandsTry"; // URI for Service we are talking to 
-//	static final String REDIRECT_URL = "http://taskapp";
+	static final String AUTHORITY_URL = "https://login.windows.net/omercantest.onmicrosoft.com";
+	static final String CLIENT_ID = "650a6609-5463-4bc4-b7c6-19df7990a8bc"; 
+	static final String RESOURCE_ID =  "https://omercantest.onmicrosoft.com/AllHandsTry"; // URI for Service we are talking to 
+	static final String REDIRECT_URL = "http://taskapp";
 	
-	static final String AUTHORITY_URL = "https://login.windows-ppe.net/p365ppetap04.ccsctp.net";
-    static final String CLIENT_ID = "a7558c9a-c964-4fbf-be19-2f277f78a586"; 
-    static final String RESOURCE_ID =  "00000002-0000-0ff1-ce00-000000000000"; // URI for Service we are talking to 
-    static final String REDIRECT_URL = "http://msopentech.com";
-    static String USER_HINT = "android@p365ppetap04.ccsctp.net";
+	
+    static String USER_HINT = "faruk@omercantest.onmicrosoft.net";
     
 	// Endpoint we are targeting for the deployed WebAPI service
-	static final String SERVICE_URL = "https://p365ppetap04.spoppe.com";
+	static final String SERVICE_URL = "https://android.azurewebsites.net";
 		
 	
 	//------------------------------------------------------------------------------------------
