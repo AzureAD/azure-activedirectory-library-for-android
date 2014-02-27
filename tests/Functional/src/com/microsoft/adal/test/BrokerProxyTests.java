31b06534add94adc0497a2cfaca65ab849e8e901
omercs <cansizoc@gmail.com>
2014-02-26 23:19:12 -0800

    intent test

--- tests/Functional/src/com/microsoft/adal/test/BrokerProxyTests.java
+++ tests/Functional/src/com/microsoft/adal/test/BrokerProxyTests.java
@@ -1,7 +1,8 @@
 
 package com.microsoft.adal.test;
 
-import static org.mockito.Matchers.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -21,7 +22,9 @@ import android.accounts.AccountManagerFuture;
 import android.accounts.AuthenticatorDescription;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -157,7 +160,7 @@ public class BrokerProxyTests extends AndroidTestCase {
         when(mockContext.getMainLooper()).thenReturn(null);
         ReflectionUtils.setFieldValue(brokerProxy, "mContext", mockContext);
         ReflectionUtils.setFieldValue(brokerProxy, "mAcctManager", mockAcctManager);
-        
+
         // action
         Method m = ReflectionUtils.getTestMethod(brokerProxy, "getAuthTokenInBackground",
                 authRequest.getClass());
@@ -182,7 +185,7 @@ public class BrokerProxyTests extends AndroidTestCase {
         when(mockContext.getMainLooper()).thenReturn(null);
         ReflectionUtils.setFieldValue(brokerProxy, "mContext", mockContext);
         ReflectionUtils.setFieldValue(brokerProxy, "mAcctManager", mockAcctManager);
-       
+
         // action
         Method m = ReflectionUtils.getTestMethod(brokerProxy, "getAuthTokenInBackground",
                 authRequest.getClass());
@@ -208,14 +211,14 @@ public class BrokerProxyTests extends AndroidTestCase {
         when(mockFuture.getResult()).thenReturn(expected);
         when(mockAcctManager.getAccountsByType(anyString())).thenReturn(accts);
         when(
-                mockAcctManager.getAuthToken(any(Account.class), anyString(), any(Bundle.class), eq(false),
-                        (AccountManagerCallback<Bundle>)eq(null), any(Handler.class))).thenReturn(mockFuture);
+                mockAcctManager.getAuthToken(any(Account.class), anyString(), any(Bundle.class),
+                        eq(false), (AccountManagerCallback<Bundle>)eq(null), any(Handler.class)))
+                .thenReturn(mockFuture);
         Context mockContext = mock(Context.class);
         when(mockContext.getMainLooper()).thenReturn(null);
         ReflectionUtils.setFieldValue(brokerProxy, "mContext", mockContext);
         ReflectionUtils.setFieldValue(brokerProxy, "mAcctManager", mockAcctManager);
-       
-        
+
         // action
         Method m = ReflectionUtils.getTestMethod(brokerProxy, "getAuthTokenInBackground",
                 authRequest.getClass());
@@ -242,13 +245,14 @@ public class BrokerProxyTests extends AndroidTestCase {
         when(mockFuture.getResult()).thenReturn(expected);
         when(mockAcctManager.getAccountsByType(anyString())).thenReturn(accts);
         when(
-                mockAcctManager.getAuthToken(any(Account.class), anyString(), any(Bundle.class), eq(false),
-                        (AccountManagerCallback<Bundle>)eq(null), any(Handler.class))).thenReturn(mockFuture);
+                mockAcctManager.getAuthToken(any(Account.class), anyString(), any(Bundle.class),
+                        eq(false), (AccountManagerCallback<Bundle>)eq(null), any(Handler.class)))
+                .thenReturn(mockFuture);
         Context mockContext = mock(Context.class);
         when(mockContext.getMainLooper()).thenReturn(null);
         ReflectionUtils.setFieldValue(brokerProxy, "mContext", mockContext);
         ReflectionUtils.setFieldValue(brokerProxy, "mAcctManager", mockAcctManager);
-               
+
         // action
         Method m = ReflectionUtils.getTestMethod(brokerProxy, "getAuthTokenInBackground",
                 authRequest.getClass());
@@ -256,8 +260,59 @@ public class BrokerProxyTests extends AndroidTestCase {
         assertEquals("token is expected", "token123", token);
     }
 
-    public void testGetIntentForBrokerActivity() {
-        // TODO
+    public void testGetIntentForBrokerActivity_emptyIntent() throws IllegalArgumentException,
+            ClassNotFoundException, NoSuchMethodException, InstantiationException,
+            IllegalAccessException, InvocationTargetException, NoSuchFieldException,
+            OperationCanceledException, AuthenticatorException, IOException {
+        Object brokerProxy = ReflectionUtils.getInstance("com.microsoft.adal.BrokerProxy");
+        Object authRequest = createAuthenticationRequest("https://login.windows.net/omercantest",
+                "resource", "client", "redirect", "loginhint", PromptBehavior.Auto, "",
+                UUID.randomUUID());
+        AccountManager mockAcctManager = mock(AccountManager.class);
+        Bundle expected = new Bundle();
+        prepareAddAccount(brokerProxy, mockAcctManager, expected);
+
+        // action
+        Method m = ReflectionUtils.getTestMethod(brokerProxy, "getIntentForBrokerActivity",
+                authRequest.getClass());
+        Intent intent = (Intent)m.invoke(brokerProxy, authRequest);
+        assertNull("intent is null", intent);
+    }
+
+    public void testGetIntentForBrokerActivity_hasIntent() throws IllegalArgumentException,
+            ClassNotFoundException, NoSuchMethodException, InstantiationException,
+            IllegalAccessException, InvocationTargetException, NoSuchFieldException,
+            OperationCanceledException, AuthenticatorException, IOException {
+        Object brokerProxy = ReflectionUtils.getInstance("com.microsoft.adal.BrokerProxy");
+        Object authRequest = createAuthenticationRequest("https://login.windows.net/omercantest",
+                "resource", "client", "redirect", "loginhint", PromptBehavior.Auto, "",
+                UUID.randomUUID());
+        AccountManager mockAcctManager = mock(AccountManager.class);
+        Bundle expected = new Bundle();
+        expected.putParcelable(AccountManager.KEY_INTENT, new Intent());
+        prepareAddAccount(brokerProxy, mockAcctManager, expected);
+
+        // action
+        Method m = ReflectionUtils.getTestMethod(brokerProxy, "getIntentForBrokerActivity",
+                authRequest.getClass());
+        Intent intent = (Intent)m.invoke(brokerProxy, authRequest);
+        assertNotNull("intent is not null", intent);
+    }
+
+    private void prepareAddAccount(Object brokerProxy, AccountManager mockAcctManager,
+            Bundle expected) throws OperationCanceledException, IOException,
+            AuthenticatorException, NoSuchFieldException, IllegalAccessException {
+        AccountManagerFuture<Bundle> mockFuture = mock(AccountManagerFuture.class);
+        when(mockFuture.getResult()).thenReturn(expected);
+        when(
+                mockAcctManager.addAccount(anyString(), anyString(), (String[])eq(null),
+                        any(Bundle.class), (Activity)eq(null),
+                        (AccountManagerCallback<Bundle>)eq(null), any(Handler.class))).thenReturn(
+                mockFuture);
+        Context mockContext = mock(Context.class);
+        when(mockContext.getMainLooper()).thenReturn(null);
+        ReflectionUtils.setFieldValue(brokerProxy, "mContext", mockContext);
+        ReflectionUtils.setFieldValue(brokerProxy, "mAcctManager", mockAcctManager);
     }
 
     private Account[] getAccountList(String accountname, String authenticatorType) {
