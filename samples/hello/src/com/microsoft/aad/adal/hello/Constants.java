fd67f4dbdd27a8bc9d092f292b5542ed18eefbf6
Omer Cansizoglu <cansizoc@gmail.com>
2014-12-17 23:27:06 -0800

    office debug

--- samples/hello/src/com/microsoft/aad/adal/hello/Constants.java
+++ samples/hello/src/com/microsoft/aad/adal/hello/Constants.java
@@ -35,13 +35,13 @@ public class Constants {
     static final String AUTHORITY_URL = "https://login.windows.net/common";
 
     // Clientid is given from AAD page when you register your Android app
-    static final String CLIENT_ID = "650a6609-5463-4bc4-b7c6-19df7990a8bc";
+    static final String CLIENT_ID = "d3590ed6-52b3-4102-aeff-aad2292ab01c";
 
     // RedirectUri
-    static final String REDIRECT_URL = "http://TaskApp";
+    static final String REDIRECT_URL = "msauth://com.microsoft.office.officehub/KY73qr6dKJ2MioJnHBaFkoQLp8Q%3D";
 
     // URI for the resource. You need to setup this resource at AAD
-    static final String RESOURCE_ID = "https://omercantest.onmicrosoft.com/AllHandsTry";
+    static final String RESOURCE_ID = "https://officeapps.live.com";
 
     // Endpoint we are targeting for the deployed WebAPI service
     static final String SERVICE_URL = "https://android.azurewebsites.net/api/values";
