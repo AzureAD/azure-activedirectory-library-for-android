0fef56931b942d126e37e1ded3700847a2d580e1
rohit <roverma@microsoft.com>
2015-02-03 15:47:55 -0800

    Reverted android-support library changes

---
+++ src/setup_dependencies.sh
@@ -0,0 +1,8 @@
+mvn install:install-file \
+ -Dfile=./libs/support-v4.jar \
+ -DgroupId=com.android.support \
+ -DartifactId=support-v4 \
+ -Dversion=\[18,\) \
+ -Dpackaging=jar \
+ -DgeneratePom=true
+
