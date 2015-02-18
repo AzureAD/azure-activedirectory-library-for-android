49e94dfaaacc9d6ad5ceae91c39718d5e42f4e4c
rohit <roverma@microsoft.com>
2015-02-18 13:48:17 -0800

    Removed script to add dependencies

--- src/setup_dependencies.sh
+++
@@ -1,8 +0,0 @@
-mvn install:install-file \
- -Dfile=./libs/support-v4.jar \
- -DgroupId=com.android.support \
- -DartifactId=support-v4 \
- -Dversion=\[18,\) \
- -Dpackaging=jar \
- -DgeneratePom=true
-
