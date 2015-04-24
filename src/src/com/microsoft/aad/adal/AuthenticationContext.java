fe34e771cff3ef4e2623aa5702c9ec72f7fc4663
omercs <cansizoc@gmail.com>
2015-04-24 16:48:30 -0700

    udpate version to 1.1.3

--- src/src/com/microsoft/aad/adal/AuthenticationContext.java
+++ src/src/com/microsoft/aad/adal/AuthenticationContext.java
@@ -1893,6 +1893,6 @@ public class AuthenticationContext {
         // Package manager does not report for ADAL
         // AndroidManifest files are not merged, so it is returning hard coded
         // value
-        return "1.1.2";
+        return "1.1.3";
     }
 }
