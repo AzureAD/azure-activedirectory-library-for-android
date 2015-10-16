84a12b93e2e1ec29ad33c0f7cee2bc5d07c0cac4
weijjia <weij@microsoft.com>
2015-10-15 19:48:05 -0700

    check if broker does not return expire date, set it to current date plus one hour

--- src/src/com/microsoft/aad/adal/BrokerProxy.java
+++ src/src/com/microsoft/aad/adal/BrokerProxy.java
@@ -21,7 +21,9 @@ package com.microsoft.aad.adal;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import android.accounts.Account;
@@ -301,7 +303,16 @@ class BrokerProxy implements IBrokerProxy {
             // IDtoken is not present in the current broker user model
             UserInfo userinfo = UserInfo.getUserInfoFromBrokerResult(bundleResult);
             final String tenantId = bundleResult.getString(AuthenticationConstants.Broker.ACCOUNT_USERINFO_TENANTID, "");
-            final Date expires = new Date(bundleResult.getLong(AuthenticationConstants.Broker.ACCOUNT_EXPIREDATE));
+            
+            final Date expires;
+            if (bundleResult.getLong(AuthenticationConstants.Broker.ACCOUNT_EXPIREDATE) == 0) {
+            	final Calendar currentTime = new GregorianCalendar();
+            	currentTime.add(Calendar.SECOND, AuthenticationConstants.DEFAULT_EXPIRATION_TIME_SEC);;
+            	expires = currentTime.getTime(); 
+            }
+            else {
+            	expires = new Date(bundleResult.getLong(AuthenticationConstants.Broker.ACCOUNT_EXPIREDATE));
+            }
             
             AuthenticationResult result = new AuthenticationResult(
                     bundleResult.getString(AccountManager.KEY_AUTHTOKEN), "", expires, false,
