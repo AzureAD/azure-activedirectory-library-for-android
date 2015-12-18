99c9c35d4823dbc996c3adf6040697befc2aa5fb
heidijinxujia <jinjia@microsoft.com>
2015-12-18 11:57:09 -0800

    Fixed a previous typo.

--- samples/hello/src/com/microsoft/aad/adal/hello/Constants.java
+++ samples/hello/src/com/microsoft/aad/adal/hello/Constants.java
@@ -28,7 +28,7 @@ public class Constants {
 
     public static final String HEADER_AUTHORIZATION = "Authorization";
 
-    public static final String HEADER_AUTHORIZATION_VALUE_PREFIX = "Bearer";
+    public static final String HEADER_AUTHORIZATION_VALUE_PREFIX = "Bearer ";
     
     // AAD PARAMETERS
     // https://login.windows.net/tenantInfo
