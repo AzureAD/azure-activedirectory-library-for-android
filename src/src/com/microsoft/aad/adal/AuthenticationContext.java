ea39f50d44a401f1e9a0647853df446270f6c1d2
weijjia <weij@microsoft.com>
2016-03-02 15:07:50 -0800

    update version for 1.1.13 release

--- src/src/com/microsoft/aad/adal/AuthenticationContext.java
+++ src/src/com/microsoft/aad/adal/AuthenticationContext.java
@@ -2057,7 +2057,7 @@ public class AuthenticationContext {
         // Package manager does not report for ADAL
         // AndroidManifest files are not merged, so it is returning hard coded
         // value
-        return "1.1.12";
+        return "1.1.13";
     }
 
     /**
