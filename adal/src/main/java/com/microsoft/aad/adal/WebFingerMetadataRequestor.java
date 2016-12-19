eadc589e30aef9db0b75ea952af067497b01f419
Brian Melton <brianmel@microsoft.com>
2016-12-19 12:21:50 -0800

    Caps changes

--- adal/src/main/java/com/microsoft/aad/adal/WebFingerMetadataRequestor.java
+++ adal/src/main/java/com/microsoft/aad/adal/WebFingerMetadataRequestor.java
@@ -20,7 +20,7 @@ class WebFingerMetadataRequestor
     WebFingerMetadata requestMetadata(WebFingerMetadataRequestParameters webFingerMetadataRequestParameters)
             throws AuthenticationException {
         final URL domain = webFingerMetadataRequestParameters.getDomain();
-        final DrsMetadata drsMetadata = webFingerMetadataRequestParameters.getDrsMetadata();
+        final DRSMetadata drsMetadata = webFingerMetadataRequestParameters.getDrsMetadata();
         Logger.v(TAG, "Validating authority for auth endpoint: " + domain.toString());
         try {
             // create the URL
@@ -71,11 +71,11 @@ class WebFingerMetadataRequestor
      * Create the URL used to retrieve the WebFinger metadata.
      *
      * @param resource    the resource to verify
-     * @param drsMetadata the {@link DrsMetadata} to consult
+     * @param drsMetadata the {@link DRSMetadata} to consult
      * @return the URL of the WebFinger document
      * @throws MalformedURLException if the URL could not be constructed
      */
-    private static URL buildWebFingerUrl(URL resource, DrsMetadata drsMetadata)
+    private static URL buildWebFingerUrl(URL resource, DRSMetadata drsMetadata)
             throws MalformedURLException {
         final URL passiveAuthEndpoint = new URL(
                 drsMetadata
