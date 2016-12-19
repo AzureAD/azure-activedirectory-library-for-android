9fe6ad73a3aba08a5c951f2c77442e57af72cbc5
Brian Melton <brianmel@microsoft.com>
2016-12-19 15:24:50 -0800

    Make URI comparisons include the port number

--- adal/src/main/java/com/microsoft/aad/adal/AdfsWebFingerValidator.java
+++ adal/src/main/java/com/microsoft/aad/adal/AdfsWebFingerValidator.java
@@ -1,6 +1,7 @@
 package com.microsoft.aad.adal;
 
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Validates trusts between authorities and ADFS instances using DRS metadata and WebFinger.
@@ -19,7 +20,16 @@ final class AdfsWebFingerValidator {
     /**
      * Constant identifying trust between two realms.
      */
-    private static final String TRUSTED_REALM_REL = "http://schemas.microsoft.com/rel/trusted-realm";
+    private static final URI TRUSTED_REALM_REL;
+
+    static {
+        try {
+            TRUSTED_REALM_REL = new URI("http://schemas.microsoft.com/rel/trusted-realm");
+        } catch (URISyntaxException e) {
+            // will not throw
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * Verify that trust is established between IDP and the SP.
@@ -28,15 +38,20 @@ final class AdfsWebFingerValidator {
      * @param metadata  the {@link WebFingerMetadata} to consult
      * @return True, if trust exists: otherwise false.
      */
-    static boolean realmIsTrusted(URL authority, WebFingerMetadata metadata) {
+    static boolean realmIsTrusted(final URI authority, final WebFingerMetadata metadata) {
         Logger.v(TAG, "Verifying trust: " + authority.toString() + metadata.toString());
         for (Link link : metadata.getLinks()) {
-            String href = link.getHref();
-            String rel = link.getRel();
-            // TODO treat this java.net.URI so that ports and stuff are preserved
-            String host = authority.getProtocol() + "://" + authority.getHost();
-            if (href.equalsIgnoreCase(host) && rel.equalsIgnoreCase(TRUSTED_REALM_REL)) {
-                return true;
+            try {
+                URI href = new URI(link.getHref());
+                URI rel = new URI(link.getRel());
+                if (href.getScheme().equalsIgnoreCase(authority.getScheme())
+                        && href.getAuthority().equalsIgnoreCase(authority.getAuthority())
+                        && rel.equals(TRUSTED_REALM_REL)) {
+                    return true;
+                }
+            } catch (URISyntaxException e) {
+                // noop
+                continue;
             }
         }
         return false;
