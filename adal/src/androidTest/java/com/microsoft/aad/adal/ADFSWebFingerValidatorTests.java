4cafc0f9f630e566282d61581512574dbabf1eae
Brian Melton <brianmel@microsoft.com>
2016-12-20 14:54:28 -0800

    ADFSWebFingerValidatorTests

--- adal/src/androidTest/java/com/microsoft/aad/adal/ADFSWebFingerValidatorTests.java
+++ adal/src/androidTest/java/com/microsoft/aad/adal/ADFSWebFingerValidatorTests.java
@@ -2,17 +2,57 @@ package com.microsoft.aad.adal;
 
 import android.test.suitebuilder.annotation.SmallTest;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
+
+import java.lang.reflect.Field;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 public class ADFSWebFingerValidatorTests extends TestCase {
 
     @SmallTest
-    public void testTrustedRealmFieldInitialized() {
-        // TODO
+    public void testTrustedRealmFieldInitialized()
+            throws NoSuchFieldException, IllegalAccessException {
+        Field trustedRealmURI = ADFSWebFingerValidator.class.getDeclaredField("TRUSTED_REALM_REL");
+        trustedRealmURI.setAccessible(true);
+        Assert.assertEquals(
+                trustedRealmURI.get(null).toString(),
+                "http://schemas.microsoft.com/rel/trusted-realm"
+        );
     }
 
     @SmallTest
-    public void testRealmIsTrusted() {
-        // TODO
+    public void testRealmIsTrustedEmptyMetadata() throws URISyntaxException {
+        final URI testAuthority = new URI("https://fs.ngctest.nttest.microsoft.com/adfs/ls/");
+        WebFingerMetadata metadata = new WebFingerMetadata();
+        Assert.assertEquals(
+                false,
+                ADFSWebFingerValidator.realmIsTrusted(
+                        testAuthority,
+                        metadata
+                )
+        );
+    }
+
+    public void testRealmIsTrusted() throws URISyntaxException {
+        final URI testAuthority = new URI("https://fs.ngctest.nttest.microsoft.com/adfs/ls/");
+        WebFingerMetadata metadata = new WebFingerMetadata();
+        metadata.setLinks(
+                new ArrayList<Link>() {{
+                    Link link = new Link();
+                    link.setHref("https://fs.ngctest.nttest.microsoft.com");
+                    link.setRel("http://schemas.microsoft.com/rel/trusted-realm");
+                    add(link);
+                }}
+        );
+        Assert.assertEquals(
+                true,
+                ADFSWebFingerValidator.realmIsTrusted(
+                        testAuthority,
+                        metadata
+                )
+        );
     }
 }
