cfc5a66313979c1732e3c72f4565996560145727
Brian Melton <brianmel@microsoft.com>
2017-09-07 11:31:27 -0400

    Adds SPE Ring data to TOkenCacheItem

--- adal/src/main/java/com/microsoft/aad/adal/TokenCacheItem.java
+++ adal/src/main/java/com/microsoft/aad/adal/TokenCacheItem.java
@@ -67,6 +67,8 @@ public class TokenCacheItem implements Serializable {
 
     private Date mExtendedExpiresOn;
 
+    private String mSpeRing;
+
     /**
      * Default constructor for cache item.
      */
@@ -87,6 +89,7 @@ public class TokenCacheItem implements Serializable {
         mTenantId = tokenCacheItem.getTenantId();
         mFamilyClientId = tokenCacheItem.getFamilyClientId();
         mExtendedExpiresOn = tokenCacheItem.getExtendedExpiresOn();
+        mSpeRing = tokenCacheItem.getSpeRing();
     }
     
     /**
@@ -112,6 +115,7 @@ public class TokenCacheItem implements Serializable {
         mRefreshtoken = authenticationResult.getRefreshToken();
         mFamilyClientId = authenticationResult.getFamilyClientId();
         mExtendedExpiresOn = authenticationResult.getExtendedExpiresOn();
+        mSpeRing = authenticationResult.getSpeRing();
     }
 
     /**
@@ -145,7 +149,7 @@ public class TokenCacheItem implements Serializable {
     public static TokenCacheItem createMRRTTokenCacheItem(final String authority, final String clientId, final AuthenticationResult authResult) {
         final TokenCacheItem item = new TokenCacheItem(authority, authResult);
         item.setClientId(clientId);
-        
+
         return item;
     }
 
@@ -434,6 +438,14 @@ public class TokenCacheItem implements Serializable {
      */
     boolean isFamilyToken() {
         return !StringExtensions.isNullOrBlank(mFamilyClientId);
+    }
+
+    String getSpeRing() {
+        return mSpeRing;
+    }
+
+    void setSpeRing(final String speRing) {
+        mSpeRing = speRing;
     }
 }
 
