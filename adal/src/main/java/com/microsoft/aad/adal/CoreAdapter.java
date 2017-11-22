5ada8f9968ab254117f0c0f63017410214b6828b
Brian Melton <brianmel@microsoft.com>
2017-11-22 15:03:36 -0500

    Adds new transformation methods for AadAccount, UserInfo

--- adal/src/main/java/com/microsoft/aad/adal/CoreAdapter.java
+++ adal/src/main/java/com/microsoft/aad/adal/CoreAdapter.java
@@ -1,5 +1,7 @@
 package com.microsoft.aad.adal;
 
+import com.microsoft.identity.common.Account;
+import com.microsoft.identity.common.internal.providers.azureactivedirectory.AzureActiveDirectoryAccount;
 import com.microsoft.identity.common.internal.providers.azureactivedirectory.AzureActiveDirectoryTokenResponse;
 
 /**
@@ -12,11 +14,44 @@ final class CoreAdapter {
     }
 
     /**
-     * Gets the supplied AuthenticationResult as a {@link AzureActiveDirectoryTokenResponse}.
+     * Gets the supplied {@link UserInfo} as an {@link Account}.
      *
-     * @return The newly created {@link AzureActiveDirectoryTokenResponse}.
+     * @param userInfo The UserInfo to transform.
+     * @return The newly created Account.
      */
-    public static AzureActiveDirectoryTokenResponse asAADTokenResponse(final AuthenticationResult result) {
+    public static AzureActiveDirectoryAccount asAadAccount(final UserInfo userInfo) {
+        final AzureActiveDirectoryAccount account = new AzureActiveDirectoryAccount();
+        account.setDisplayableId(userInfo.getDisplayableId());
+        account.setName(userInfo.getGivenName());
+        account.setIdentityProvider(account.getIdentityProvider());
+        account.setUid(userInfo.getUserId());
+        // TODO Need to get the UTID attribute.
+        return account;
+    }
+
+    /**
+     * Gets the supplied {@link Account} as a {@link UserInfo}.
+     *
+     * @param account The Account to transform.
+     * @return The newly created UserInfo.
+     */
+    public static UserInfo asUserInfo(final AzureActiveDirectoryAccount account) {
+        return new UserInfo(
+                account.getUserIdentifier(),
+                account.getName(),
+                null, // TODO Need to get the 'family name' attribute
+                account.getIdentityProvider(),
+                account.getDisplayableId()
+        );
+    }
+
+    /**
+     * Gets the supplied {@link AuthenticationResult} as a {@link AzureActiveDirectoryTokenResponse}.
+     *
+     * @param result The AuthenticationResult to transform.
+     * @return The newly created AzureActiveDirectoryTokenResponse.
+     */
+    public static AzureActiveDirectoryTokenResponse asAadTokenResponse(final AuthenticationResult result) {
         final AzureActiveDirectoryTokenResponse adTokenResponse = new AzureActiveDirectoryTokenResponse();
         adTokenResponse.setAccessToken(result.getAccessToken());
         adTokenResponse.setTokenType(result.getAccessTokenType());
