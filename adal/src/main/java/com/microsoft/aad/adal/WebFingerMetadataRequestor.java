da9b0ddeab1cc92603d3b9058e8ab5fa7abc3614
Ping Qiu <piqiu@microsoft.com>
2017-12-19 15:06:52 -0800

    fix android studio 3.0 build issue by upgrading gradle version from v2.14.1 to v3.5 (#1081)
    * fix android studio 3.0 build issue by upgrading gradle version from v2.14.1 to v3.5
    * fix the PMD issue

--- adal/src/main/java/com/microsoft/aad/adal/WebFingerMetadataRequestor.java
+++ adal/src/main/java/com/microsoft/aad/adal/WebFingerMetadataRequestor.java
@@ -99,6 +99,7 @@ class WebFingerMetadataRequestor
      * @return the URL of the WebFinger document
      * @throws MalformedURLException if the URL could not be constructed
      */
+    @SuppressWarnings("PMD")
     static URL buildWebFingerUrl(final URL resource, final DRSMetadata drsMetadata)
             throws MalformedURLException {
         final URL passiveAuthEndpoint = new URL(
