2d499473d4ad91c5b6ddb2a842961a7126286e64
unknown <jinjia@MININT-BPVLVN1.northamerica.corp.microsoft.com>
2018-01-02 17:12:47 -0800

    Add unit test for "&CertAuthorities=" scenario.

--- adal/src/androidTest/java/com/microsoft/aad/adal/ChallengeResponseBuilderTests.java
+++ adal/src/androidTest/java/com/microsoft/aad/adal/ChallengeResponseBuilderTests.java
@@ -308,6 +308,13 @@ public class ChallengeResponseBuilderTests extends AndroidTestHelper {
 
         try {
             m.invoke(handler, CERT_REDIRECT
+                    + "?Nonce=2&CertAuthorities=&Version=1.0&SubmitUrl=1&Context=1");
+        } catch (Exception ex) {
+            Assert.fail("No exception");
+        }
+
+        try {
+            m.invoke(handler, CERT_REDIRECT
                     + "?Nonce=2&CertAuthorities=ABC&Version=1.0&SubmitUrlMissing=1&Context=1");
             Assert.fail("No exception");
         } catch (Exception ex) {
