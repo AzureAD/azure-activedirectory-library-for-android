c32d1abe5197065b3ea6797f6adbb6ca4f7b7b97
v-dmkapi <v-dmkapi@microsoft.com>
2018-03-27 14:22:30 -0700

    Adding scrolling to functionality of searching for apps. Also created new algo to scroll up, since UIScrollable class didnt have one.

--- automationtestapp/src/androidTest/java/com/microsoft/aad/automation/testapp/UIAutomatorTests/SampleFunctions.java
+++ automationtestapp/src/androidTest/java/com/microsoft/aad/automation/testapp/UIAutomatorTests/SampleFunctions.java
@@ -3,6 +3,7 @@ package com.microsoft.aad.automation.testapp.UIAutomatorTests;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 
 
@@ -63,6 +64,18 @@ public class SampleFunctions {
         }
     }
 
+    private static void click_and_await_resourceID(String resourceID)
+    {
+        set_device();
+        UiObject appButton = mDevice.findObject(new UiSelector()
+                .resourceId(resourceID));
+        try{
+            appButton.clickAndWaitForNewWindow(WINDOW_TIMEOUT);
+        } catch (UiObjectNotFoundException e) {
+            Assert.fail("Didn't find the resourceID to click on: " + resourceID);
+        }
+    }
+
     private static void set_text_by_Text(String UIElementText,String text_to_set)
     {
         set_device();
@@ -82,7 +95,7 @@ public class SampleFunctions {
         }
     }
 
-    private static void open_Applications_View()
+    public static void open_Applications_View()
     {
         set_device();
         // press the home button
@@ -105,9 +118,6 @@ public class SampleFunctions {
         open_Applications_View();
         click_by_Text(mAppName);
     }
-
-
-
 
     public static void uninstall_App(String AppName)
     {
@@ -163,30 +173,65 @@ public class SampleFunctions {
         click_by_resourceID("com.android.packageinstaller:id/permission_allow_button");
     }
 
-    public static void enroll_authenticator_personal(String UPN,String Secret,Boolean Federated)
+    public static void scroll_findapp(String textToClick){
+        open_Applications_View();
+        scroll_up();
+        set_device();
+
+        try {
+            UiScrollable appView = new UiScrollable(new UiSelector().scrollable(true));
+            appView.scrollIntoView(new UiSelector().text(textToClick));
+            mDevice.findObject(new UiSelector().text(textToClick)).clickAndWaitForNewWindow();
+        } catch (UiObjectNotFoundException e) {
+
+        }
+    }
+
+    public static void scroll_up()
+    {
+        set_device();
+        int H = mDevice.getDisplayHeight();
+        int W = mDevice.getDisplayWidth();
+        int x = W / 2;
+        double y1 = H * .25;
+        double y2 =  H * .75;
+
+        mDevice.swipe(x,(int)y1,x,(int)y2,5);
+    }
+
+    public static void enroll_authenticator(String UPN,String Secret,Boolean Federated)
     {
         mFederated = Federated;
         mUPN = UPN;
         mSecret = Secret;
 
         launch_App(AUTHENTICATOR_APP_NAME);
-        click_by_resourceID("com.azure.authenticator:id/next");
-        click_by_resourceID("com.azure.authenticator:id/next");
-        click_by_resourceID("com.azure.authenticator:id/done");
-        click_by_resourceID("com.azure.authenticator:id/zero_accounts_add_account_button");
-        //personal account
-        click_by_resourceID("com.azure.authenticator:id/add_account_personal_btn");
-        // OTHER_ACCOUNT click_by_resourceID("com.azure.authenticator:id/add_account_other_btn");
-        // WORK_ACCOUNT click_by_resourceID("com.azure.authenticator:id/add_account_work_btn");
+        click_by_Text("SKIP");
+        click_by_resourceID("com.azure.authenticator:id/menu_overflow");
+        click_by_Text("Settings");
+        click_by_Text("Register your device with your organization");
+        allow_permission();
+        set_text_by_Text("Organization email",mUPN);
+        click_and_await_resourceID("com.azure.authenticator:id/manage_device_registration_register_button");
         authenticate_webview();
     }
 
     private static void authenticate_webview()
     {
-        set_text_by_Text("Enter your email, phone, or Skype.",mUPN);
-        click_and_await("Next");
+        //set_text_by_Text("Enter your email, phone, or Skype.",mUPN);
+        //click_and_await("Next");
+        set_text_by_Text("Enter password",mSecret);
+        click_and_await("Sign in");
     }
 
+    public static void remove_authenticator_account()
+    {
+        launch_App(AUTHENTICATOR_APP_NAME);
+        click_by_resourceID("com.azure.authenticator:id/menu_overflow");
+        click_by_Text("Edit accounts");
+        click_by_resourceID("com.azure.authenticator:id/account_list_row_delete");
+        click_by_Text("Remove account");
+    }
 
     public static void unenroll_authenticator()
     {
