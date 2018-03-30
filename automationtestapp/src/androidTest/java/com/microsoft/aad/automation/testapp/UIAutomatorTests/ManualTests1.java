2ccb25e929a41d606bcc19ff78891475005e3714
v-dmkapi <v-dmkapi@microsoft.com>
2018-03-30 10:44:39 -0700

    Adding scrolling to functionality of searching for apps. Also created new algo to scroll up, since UIScrollable class didnt have one.

--- automationtestapp/src/androidTest/java/com/microsoft/aad/automation/testapp/UIAutomatorTests/ManualTests1.java
+++ automationtestapp/src/androidTest/java/com/microsoft/aad/automation/testapp/UIAutomatorTests/ManualTests1.java
@@ -13,15 +13,24 @@ import android.support.test.runner.AndroidJUnit4;
 public class ManualTests1 {
 
     @Test
-    public void clear_data()
+    public void testing()
     {
-        SampleFunctions.clear_app_data("Authenticator");
+        SampleFunctions.find_click("Apps");
     }
 
     @Test
-    public void test_scroll() {
-        //SampleFunctions.open_Applications_View();
-        SampleFunctions.launch_App("adalR");
+    public void enroll()
+    {
+        //SampleFunctions.launch_App("Authenticator");
+        SampleFunctions.enroll_authenticator(
+                "",
+                ""
+        );
+    }
+
+    @Test
+    public void un_enroll() {
+        SampleFunctions.remove_authenticator_account();
     }
 
     @Test
@@ -38,5 +47,50 @@ public class ManualTests1 {
 
         SampleFunctions.clear_app_data("Authenticator");
        //SampleFunctions.uninstall_App("adalR");
+    }
+
+
+    @Test
+    public void clear_data()
+    {
+        SampleFunctions.clear_app_data("adalR");
+        SampleFunctions.clear_app_data("adal");
+    }
+
+
+    @Test
+    public void test_case()
+    {
+
+        String upn = "";
+        String secret = "";
+
+        //SampleFunctions.enroll_authenticator(upn, secret);
+
+        //SampleFunctions.launch_App("adal");
+        //SampleFunctions.adal_clear_cache();
+        SampleFunctions.launch_App("adal");
+        //SampleFunctions.adal_clear_cache();
+
+        // this code block acquires a token via automationtest app.
+        SampleFunctions.click_and_await("Acquire Token");
+        String json = SampleFunctions.json_body(
+                "https://login.microsoftonline.com/common",
+                "00000002-0000-0000-c000-000000000000",
+                "urn:ietf:wg:oauth:2.0:oob",
+                "4b0db8c2-9f26-4417-8bde-3f0e3656f8e0"
+        );
+
+        SampleFunctions.set_text_by_Text("Please enter the sign in info here...",json);
+        SampleFunctions.click_and_await("Go");
+        SampleFunctions.authenticate_webview2(upn,secret);
+        SampleFunctions.click_and_await("Done");
+
+    }
+
+    @Test
+    public void test_download() {
+        SampleFunctions.download_install_app("Microsoft Authenticator");
+        SampleFunctions.download_install_app("Intune Company Portal");
     }
 }
