ed1d562786ffce3c1f0d981f5afb1eaa780ba44c
danieldobalian <ddobalian@gmail.com>
2018-04-12 17:53:30 -0700

    correcting behavior description for claims overloads

--- adal/src/main/java/com/microsoft/aad/adal/AuthenticationContext.java
+++ adal/src/main/java/com/microsoft/aad/adal/AuthenticationContext.java
@@ -440,7 +440,7 @@ public class AuthenticationContext {
 
     /**
      * acquireToken will start an interactive auth flow to acquire new tokens 
-     * with the requested claims. Bypasses token cache. 
+     * with the requested claims. Bypasses token cache if promptbehavior is not AUTO or claims are passed. 
      *
      * @param activity             Calling activity
      * @param resource             required resource identifier.
@@ -520,7 +520,7 @@ public class AuthenticationContext {
 
     /**
      * acquireToken will start an interactive auth flow to acquire new tokens 
-     * with the requested claims. Bypasses token cache. 
+     * with the requested claims. Bypasses token cache if promptbehavior is not AUTO or claims are passed. 
      *
      * @param fragment             It accepts both type of fragments.
      * @param resource             required resource identifier.
@@ -602,7 +602,7 @@ public class AuthenticationContext {
 
     /**
      * acquireToken will start an interactive auth flow to acquire new tokens 
-     * with the requested claims. Bypasses token cache. This overload uses new dialog based prompt. 
+     * with the requested claims. Bypasses token cache if promptbehavior is not AUTO or claims are passed. This overload uses new dialog based prompt. 
      * It will create a handler to run the dialog related code. 
      *
      * @param resource             required resource identifier.
