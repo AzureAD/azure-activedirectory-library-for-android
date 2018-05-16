f7ac0b23287d0ce4b04ae0dd7d9e5e95c3362d82
kreedula <kreedula@microsoft.com>
2018-05-16 00:34:13 -0700

    Added Changes to SignInActivity to ensure we can pass token cache result to expire token

--- automation/src/test/java/com/microsoft/identity/common/test/automation/questions/AccessToken.java
+++ automation/src/test/java/com/microsoft/identity/common/test/automation/questions/AccessToken.java
@@ -1,5 +1,6 @@
 package com.microsoft.identity.common.test.automation.questions;
 
+import com.microsoft.identity.common.test.automation.actors.User;
 import com.microsoft.identity.common.test.automation.model.ReadCacheResult;
 import com.microsoft.identity.common.test.automation.model.ResultsMapper;
 import com.microsoft.identity.common.test.automation.ui.Results;
@@ -13,6 +14,8 @@ public class AccessToken implements Question<String> {
     public String answeredBy(Actor actor) {
         String results = Text.of(Results.RESULT_FIELD).viewedBy(actor).asString();
         ReadCacheResult readCacheResult = ResultsMapper.GetReadCacheResultFromString(results);
+        User user = (User) actor;
+        user.setCacheResult(readCacheResult.tokenCacheItems.get(0));
         return readCacheResult.tokenCacheItems.get(0).accessToken;
     }
 
