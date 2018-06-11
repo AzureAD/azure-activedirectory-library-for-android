01f809768fadf4ab78ef8a4561240b4b9b3e8d0a
Shane Oatman <shoatman@microsoft.com>
2018-06-11 12:56:53 -0700

    Updates to automation app for testing w/ debug build of authenticator

--- automation/src/test/java/com/microsoft/identity/common/test/automation/tasks/EnterUserNameForSignInDisambiguation.java
+++ automation/src/test/java/com/microsoft/identity/common/test/automation/tasks/EnterUserNameForSignInDisambiguation.java
@@ -2,6 +2,7 @@ package com.microsoft.identity.common.test.automation.tasks;
 
 import com.microsoft.identity.common.test.automation.actors.User;
 import com.microsoft.identity.common.test.automation.ui.identityproviders.AADV1.SignInPageUserName;
+import com.microsoft.identity.common.test.automation.ui.identityproviders.AADV1.SignInPageUserNameBroker;
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
@@ -14,14 +15,30 @@ import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisi
 
 public class EnterUserNameForSignInDisambiguation implements Task {
 
+    Boolean withBroker;
+
     @Override
     public <T extends Actor> void performAs(T actor) {
         User user = (User)actor;
-        user.attemptsTo(
-                WaitUntil.the(SignInPageUserName.USERNAME, isVisible()).forNoMoreThan(10).seconds(),
-                Enter.theValue(user.getCredential().userName).into(SignInPageUserName.USERNAME),
-                Click.on(SignInPageUserName.NEXT_BUTTON)
-        );
+
+        if(withBroker){
+            user.attemptsTo(
+                    WaitUntil.the(SignInPageUserNameBroker.USERNAME, isVisible()).forNoMoreThan(10).seconds(),
+                    Enter.theValue(user.getCredential().userName).into(SignInPageUserNameBroker.USERNAME),
+                    Click.on(SignInPageUserNameBroker.NEXT_BUTTON)
+            );
+        }else {
+            user.attemptsTo(
+                    WaitUntil.the(SignInPageUserName.USERNAME, isVisible()).forNoMoreThan(10).seconds(),
+                    Enter.theValue(user.getCredential().userName).into(SignInPageUserName.USERNAME),
+                    Click.on(SignInPageUserName.NEXT_BUTTON)
+            );
+        }
+    }
+
+    public EnterUserNameForSignInDisambiguation withBroker(Boolean withBroker){
+        this.withBroker = withBroker;
+        return this;
     }
 
 }
