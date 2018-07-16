512499966d409aad08076de107652fd5e886212c
Krishna Eedula <38264932+kreedula@users.noreply.github.com>
2018-07-16 14:01:54 -0700

    Updated version to 1.15.0-RC (#1252)
    * Updated version to 1.15.0-RC
    
    * Updated change log
    
    * Modified change log
    
    * Added transitive=false for dist implementation flavor

--- adal/src/main/java/com/microsoft/aad/adal/AuthenticationContext.java
+++ adal/src/main/java/com/microsoft/aad/adal/AuthenticationContext.java
@@ -1423,7 +1423,7 @@ public class AuthenticationContext {
         // Package manager does not report for ADAL
         // AndroidManifest files are not merged, so it is returning hard coded
         // value
-        return "1.14.0";
+        return "1.15.0-RC";
     }
 
     /**
