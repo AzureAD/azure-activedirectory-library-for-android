72387ca267c55b1a53c341065e4ff3654e204b92
Brian Melton-Grace <brianmel@microsoft.com>
2019-07-29 12:31:58 -0700

    Removing duplication around PackageHelper

--- adal/src/main/java/com/microsoft/aad/adal/PackageHelper.java
+++ adal/src/main/java/com/microsoft/aad/adal/PackageHelper.java
@@ -23,33 +23,12 @@
 
 package com.microsoft.aad.adal;
 
-import android.accounts.AccountManager;
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.Signature;
-import android.util.Base64;
-
-import com.microsoft.identity.common.adal.internal.AuthenticationConstants;
-import com.microsoft.identity.common.adal.internal.util.StringExtensions;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * Gets information about calling activity.
  */
-public class PackageHelper {
-    private static final String TAG = "CallerInfo";
-
-    private Context mContext;
-
-    private final AccountManager mAcctManager; //NOPMD
+public class PackageHelper extends com.microsoft.identity.common.internal.broker.PackageHelper {
 
     /**
      * Creates helper to check caller info.
@@ -57,87 +36,6 @@ public class PackageHelper {
      * @param ctx The android app/activity context
      */
     public PackageHelper(Context ctx) {
-        mContext = ctx;
-        mAcctManager = AccountManager.get(mContext);
-    }
-
-    /**
-     * Reads first signature in the list for given package name.
-     *
-     * @param packagename name of the package for which signature should be returned
-     * @return signature for package
-     */
-    @SuppressLint("PackageManagerGetSignatures")
-    public String getCurrentSignatureForPackage(final String packagename) {
-        try {
-            PackageInfo info = mContext.getPackageManager().getPackageInfo(packagename,
-                    PackageManager.GET_SIGNATURES);
-            if (info != null && info.signatures != null && info.signatures.length > 0) {
-                Signature signature = info.signatures[0];
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                return Base64.encodeToString(md.digest(), Base64.NO_WRAP);
-                // Server side needs to register all other tags. ADAL will
-                // send one of them.
-            }
-        } catch (NameNotFoundException e) {
-            Logger.e(TAG, "Calling App's package does not exist in PackageManager. ", "",
-                    ADALError.APP_PACKAGE_NAME_NOT_FOUND);
-        } catch (NoSuchAlgorithmException e) {
-            Logger.e(TAG, "Digest SHA algorithm does not exists. ", "",
-                    ADALError.DEVICE_NO_SUCH_ALGORITHM);
-        }
-        return null;
-    }
-
-    /**
-     * Gets the kernel user-ID that has been assigned to this application.
-     *
-     * @param packageName for which the user id has to be returned
-     * @return UID user id
-     */
-    public int getUIDForPackage(final String packageName) {
-        int callingUID = 0;
-        try {
-            final ApplicationInfo info = mContext.getPackageManager().getApplicationInfo(packageName, 0);
-            if (info != null) {
-                callingUID = info.uid;
-            }
-        } catch (NameNotFoundException e) {
-            Logger.e(TAG, "Package is not found. ", "Package name: " + packageName,
-                    ADALError.PACKAGE_NAME_NOT_FOUND, e);
-        }
-        return callingUID;
-    }
-
-    /**
-     * Gets redirect uri for broker.
-     *
-     * @param packageName     application package name
-     * @param signatureDigest application signature
-     * @return broker redirect url
-     */
-    public static String getBrokerRedirectUrl(final String packageName, final String signatureDigest) {
-        if (!StringExtensions.isNullOrBlank(packageName)
-                && !StringExtensions.isNullOrBlank(signatureDigest)) {
-            // If the caller is the Authenticator, then use the broker redirect URI.
-            if (packageName.equals(AuthenticationConstants.Broker.AZURE_AUTHENTICATOR_APP_PACKAGE_NAME) &&
-                    signatureDigest.equals(AuthenticationConstants.Broker.AZURE_AUTHENTICATOR_APP_SIGNATURE)) {
-                return AuthenticationConstants.Broker.BROKER_REDIRECT_URI;
-            }
-
-            try {
-                return String.format("%s://%s/%s", AuthenticationConstants.Broker.REDIRECT_PREFIX,
-                        URLEncoder.encode(packageName, AuthenticationConstants.ENCODING_UTF8),
-                        URLEncoder.encode(signatureDigest, AuthenticationConstants.ENCODING_UTF8));
-            } catch (UnsupportedEncodingException e) {
-                // This encoding issue will happen at the beginning of API call,
-                // if it is not supported on this device. ADAL uses one encoding
-                // type.
-                Logger.e(TAG, ADALError.ENCODING_IS_NOT_SUPPORTED.getDescription(), "",
-                        ADALError.ENCODING_IS_NOT_SUPPORTED, e);
-            }
-        }
-        return "";
+        super(ctx.getPackageManager());
     }
 }
