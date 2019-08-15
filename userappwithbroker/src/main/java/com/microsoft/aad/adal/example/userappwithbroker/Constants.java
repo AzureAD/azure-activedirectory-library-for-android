8ac4ec158ce670868fbc6276c6087a9a7a1350c5
Brian Melton-Grace <brianmel@microsoft.com>
2019-08-15 15:23:59 -0700

    Removing MSDEVEX clientid and swapping for MSIDLABS

--- userappwithbroker/src/main/java/com/microsoft/aad/adal/example/userappwithbroker/Constants.java
+++ userappwithbroker/src/main/java/com/microsoft/aad/adal/example/userappwithbroker/Constants.java
@@ -58,7 +58,7 @@ public class Constants {
 
 
     enum ClientId {
-        MSDEVEX("b92e0ba5-f86e-4411-8e18-6b5f928d968a"),
+        MSIDLABS("4b0db8c2-9f26-4417-8bde-3f0e3656f8e0"),
         ONEDRIVE("af124e86-4e96-495a-b70a-90f90ab96707"),
         OFFICE("d3590ed6-52b3-4102-aeff-aad2292ab01c"),
         APPCHECK2_BF("f5d01c1c-abe6-4207-ae2d-5bc9af251724"),
