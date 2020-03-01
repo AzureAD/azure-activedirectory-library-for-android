3750a828cc125c7130e42f5a890f211925451a70
Dome Pongmongkol <rapong@microsoft.com>
2020-03-01 15:50:32 -0800

    fix issues in test app regarding sov cloud

--- userappwithbroker/src/main/java/com/microsoft/aad/adal/example/userappwithbroker/MainActivity.java
+++ userappwithbroker/src/main/java/com/microsoft/aad/adal/example/userappwithbroker/MainActivity.java
@@ -275,7 +275,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
                         // Update this user for next call
                         if (authenticationResult.getUserInfo() != null) {
-                            saveUserIdFromAuthenticationResult(authenticationResult, mRequestAuthority);
+                            saveUserIdFromAuthenticationResult(authenticationResult);
                         }
                     }
 
@@ -291,21 +291,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
      * for later use.
      * To make the sample app easier, the saved data will be keyed by displayable id.
      */
-    private void saveUserIdFromAuthenticationResult(final AuthenticationResult authResult, final String authority) {
+    private void saveUserIdFromAuthenticationResult(final AuthenticationResult authResult) {
         mSharedPreference = getSharedPreferences(SHARED_PREFERENCE_STORE_USER_UNIQUEID, MODE_PRIVATE);
         if (null != authResult) {
             if (null != authResult.getAuthority()) {
                 final SharedPreferences.Editor prefEditor = mSharedPreference.edit();
                 if (null != authResult.getUserInfo() && null != authResult.getUserInfo().getDisplayableId()) {
-                    if (null != authResult.getUserInfo() && null != authResult.getUserInfo().getIdentityProvider()) {
-                        prefEditor.putString(
-                                (authResult.getUserInfo().getDisplayableId().trim() + ":" + authority.trim() +  ":authority").toLowerCase(),
-                                authResult.getUserInfo().getIdentityProvider().trim().toLowerCase());
-                    } else {
-                       prefEditor.putString(
-                               (authResult.getUserInfo().getDisplayableId().trim() + ":" + authority.trim() +  ":authority").toLowerCase(),
-                               authResult.getAuthority().trim().toLowerCase());
-                    }
+                    prefEditor.putString(
+                            (authResult.getUserInfo().getDisplayableId().trim() + ":" + authResult.getAuthority() +  ":authority").toLowerCase(),
+                            authResult.getAuthority().trim().toLowerCase());
                 }  else {
                     final Toast toast = Toast.makeText(mApplicationContext,
                             "Warning: the result authority is null," +
@@ -316,7 +310,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 if (null != authResult.getUserInfo()
                     && null != authResult.getUserInfo().getDisplayableId()
                     && null != authResult.getUserInfo().getUserId()) {
-                    prefEditor.putString((authResult.getUserInfo().getDisplayableId().trim() + ":" + authority.trim() + ":userId").toLowerCase(), authResult.getUserInfo().getUserId().trim().toLowerCase());
+                    prefEditor.putString((authResult.getUserInfo().getDisplayableId().trim() + ":" + authResult.getAuthority() + ":userId").toLowerCase(),
+                            authResult.getUserInfo().getUserId().trim().toLowerCase());
                 } else {
                     final Toast toast = Toast.makeText(mApplicationContext,
                             "Warning: the result userInfo is null. " +
