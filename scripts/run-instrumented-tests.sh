08999652695bc759d6dafd9657a6e009ecb938f5
Shane Oatman <shoatman@microsoft.com>
2020-12-29 13:29:17 -0800

    Updated script

--- scripts/run-instrumented-tests.sh
+++ scripts/run-instrumented-tests.sh
@@ -9,5 +9,5 @@ gradle -version
 echo =============================================
 echo Running instrumented tests
 echo =============================================
-gradle common:connectedDebugAndroidTest -i
+gradle adal:connectedDebugAndroidTest -i
 
