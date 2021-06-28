d28db750e91b879ed14a6e81753fadd6a95a9636
Brian Melton-Grace <brianmel@microsoft.com>
2021-06-28 15:18:26 -0700

    Bugfix for ADAL cache initialization

--- adal/src/main/java/com/microsoft/aad/adal/DefaultTokenCacheStore.java
+++ adal/src/main/java/com/microsoft/aad/adal/DefaultTokenCacheStore.java
@@ -102,7 +102,12 @@ public class DefaultTokenCacheStore implements ITokenCacheStore, ITokenStoreQuer
             }
         }
 
-        mPrefs = new SharedPreferencesFileManager(mContext, SHARED_PREFERENCE_NAME);
+        mPrefs = SharedPreferencesFileManager.getSharedPreferences(
+                mContext,
+                SHARED_PREFERENCE_NAME,
+                Context.MODE_PRIVATE,
+                null
+        );
 
         // Check upfront when initializing DefaultTokenCacheStore. 
         // If it's under API 18 and secretkey is not provided, we should fail upfront to inform 
