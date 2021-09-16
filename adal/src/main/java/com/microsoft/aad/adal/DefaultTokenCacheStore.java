f2b4c39040d062e303faa8b4c53079d3325578d9
Ahmet Erciyas <65786706+aherciya@users.noreply.github.com>
2021-09-16 10:56:55 -0500

    reverting to no changes

--- adal/src/main/java/com/microsoft/aad/adal/DefaultTokenCacheStore.java
+++ adal/src/main/java/com/microsoft/aad/adal/DefaultTokenCacheStore.java
@@ -169,7 +169,7 @@ public class DefaultTokenCacheStore implements ITokenCacheStore, ITokenStoreQuer
             String json = mPrefs.getString(key);
             json = null != json ? json : "";
             String decrypted = decrypt(key, json);
-            if (validTokenCacheItem(decrypted)) {
+            if (decrypted == null) {
                 try {
                     return mGson.fromJson(decrypted, TokenCacheItem.class);
                 } catch (final JsonSyntaxException exception) {
@@ -238,7 +238,7 @@ public class DefaultTokenCacheStore implements ITokenCacheStore, ITokenStoreQuer
             final String tokenValue = tokenEntry.getValue();
 
             final String decryptedValue = decrypt(tokenKey, tokenValue);
-            if (validTokenCacheItem(decryptedValue)) {
+            if (decryptedValue == null) {
                 try {
                     final TokenCacheItem tokenCacheItem = mGson.fromJson(decryptedValue, TokenCacheItem.class);
                     tokens.add(tokenCacheItem);
@@ -389,13 +389,6 @@ public class DefaultTokenCacheStore implements ITokenCacheStore, ITokenStoreQuer
         }
 
         return mPrefs.contains(key);
-    }
-
-    private boolean validTokenCacheItem(String item){
-        if (item == null) {
-            Logger.w(TAG, "Bad input, was null. ");
-            return false;
-        }
     }
 
 }
